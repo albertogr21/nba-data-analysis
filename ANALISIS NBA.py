9a168b2c38f11e6dd8525260cf06973ee994cea7
albertogr21 <albertitocrash21@hotmail.com>
2025-08-04 02:29:46 +0200

    Update ANALISIS NBA.py

--- ANALISIS NBA.py
+++ ANALISIS NBA.py
@@ -2,18 +2,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Cargar datos
+
 df = pd.read_csv('nba_2025_per_game.csv')
 
-# Revisar estructura
+
 print(df.info())
 print(df.describe())
 
-# Top 10 anotadores
+
 top_scorers = df.sort_values(by='PTS', ascending=False).head(10)
 print("Top 10 anotadores:\n", top_scorers[['Player', 'PTS', 'G']])
 
-# Visualización: Top 10 jugadores por puntos por partido
+
 plt.figure(figsize=(12, 6))
 sns.barplot(x='PTS', y='Player', data=top_scorers, palette='viridis')
 plt.title('Top 10 Jugadores - Puntos por Partido (NBA 2025)')
@@ -22,7 +22,7 @@ plt.ylabel('Jugador')
 plt.tight_layout()
 plt.show()
 
-# Relación entre minutos jugados y puntos
+
 plt.figure(figsize=(10, 6))
 sns.barplot(x='PTS', y='Player', data=top_scorers, palette='viridis', legend=False)
 plt.title('Minutos vs Puntos por Partido')
@@ -32,8 +32,6 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
-# Correlación entre estadísticas clave
-# Correlación entre estadísticas clave
 correlation = df[['PTS', 'AST', 'TRB', 'STL', 'BLK']].corr()
 sns.heatmap(correlation, annot=True, cmap='coolwarm')
 plt.title('Correlación entre estadísticas clave')
